474adacc086731ece2362518ea3ac993e965730e
Brodie <brodie.w.mackenzie@gmail.com>
2022-01-21 19:46:56 +0000

    add service account, grant roles for org and projects

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "random_id" "random_project_id_suffix" {
 }
 
 /**********************************************
-Create prequisite projects
+Create prerequisite projects
 ***********************************************/
 
 resource "google_project" "terraform_seed_project" {
@@ -73,10 +73,8 @@ resource "google_project" "confidential_project" {
 }
 
 /**********************************************
-Enable APIs in prequisite projects
+Enable APIs in prerequisite projects
 ***********************************************/
-
-# TODO - Fix billing account enablement prerequisite for activating APIs
 
 variable "terraform_seed_project_service_list" {
   description ="The list of APIs to enable for the terraform seed project"
@@ -202,3 +200,140 @@ resource "google_project_service" "confidential_project_services" {
   project = google_project.confidential_project.project_id
   service = each.key
 }
+
+/**********************************************
+Create privileged service account
+***********************************************/
+
+resource "google_service_account" "terraform_service_account" {
+  project = google_project.terraform_seed_project.project_id
+  account_id   = "sa-tf-seed-${random_id.random_project_id_suffix.hex}"
+  display_name = "Terraform Seed Service Account"
+}
+
+/**********************************************
+Grant roles to privileged service account at the org level
+***********************************************/
+
+variable "terraform_service_account_org_iam_roles_list" {
+  description ="The list of roles to grant to the terraform service account at the org level"
+  type = list(string)
+  default = [
+    "roles/accesscontextmanager.policyAdmin",
+    "roles/orgpolicy.policyAdmin",
+    "roles/resourcemanager.organizationAdmin"
+]
+}
+
+resource "google_organization_iam_member" "terraform_service_account_org_iam" {
+  for_each = toset(var.terraform_service_account_org_iam_roles_list)
+  org_id  = "898267907536"
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.terraform_service_account.email}"
+}
+
+/**********************************************
+Grant roles to privileged service account for the projects
+***********************************************/
+
+variable "terraform_service_account_data_ingestion_iam_roles_list" {
+  description ="The list of roles to grant to the terraform service account for the data ingestion project"
+  type = list(string)
+  default = [
+    "roles/appengine.appCreator",
+    "roles/cloudscheduler.admin",
+    "roles/compute.networkAdmin",
+    "roles/compute.securityAdmin",
+    "roles/dataflow.developer",
+    "roles/dns.admin",
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/pubsub.admin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.serviceAccountTokenCreator",
+    "roles/serviceusage.serviceUsageAdmin",
+    "roles/storage.admin"
+]
+}
+
+resource "google_project_iam_member" "terraform_service_account_data_ingestion_iam" {
+  for_each = toset(var.terraform_service_account_data_ingestion_iam_roles_list)
+  project = google_project.data_ingestion_project.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.terraform_service_account.email}"
+}
+
+variable "terraform_service_account_data_governance_iam_roles_list" {
+  description ="The list of roles to grant to the terraform service account for the data governance project"
+  type = list(string)
+  default = [
+    "roles/cloudkms.admin",
+    "roles/cloudkms.cryptoKeyEncrypter",
+    "roles/dlp.deidentifyTemplatesEditor",
+    "roles/dlp.inspectTemplatesEditor",
+    "roles/dlp.user",
+    "roles/datacatalog.admin",
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/secretmanager.admin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.serviceAccountTokenCreator",
+    "roles/serviceusage.serviceUsageAdmin",
+    "roles/storage.admin"
+]
+}
+
+resource "google_project_iam_member" "terraform_service_account_data_governance_iam" {
+  for_each = toset(var.terraform_service_account_data_governance_iam_roles_list)
+  project = google_project.data_governance_project.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.terraform_service_account.email}"
+}
+
+variable "terraform_service_account_non_confidential_iam_roles_list" {
+  description ="The list of roles to grant to the terraform service account for the non-confidential project"
+  type = list(string)
+  default = [
+    "roles/bigquery.admin",
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.serviceAccountTokenCreator",
+    "roles/serviceusage.serviceUsageAdmin",
+    "roles/storage.admin"
+]
+}
+
+resource "google_project_iam_member" "terraform_service_account_non_confidential_iam" {
+  for_each = toset(var.terraform_service_account_non_confidential_iam_roles_list)
+  project = google_project.non_confidential_project.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.terraform_service_account.email}"
+}
+
+variable "terraform_service_account_confidential_iam_roles_list" {
+  description ="The list of roles to grant to the terraform service account for the confidential project"
+  type = list(string)
+  default = [
+    "roles/bigquery.admin",
+    "roles/compute.networkAdmin",
+    "roles/compute.securityAdmin",
+    "roles/dns.admin",
+    "roles/dataflow.developer",
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.serviceAccountTokenCreator",
+    "roles/serviceusage.serviceUsageAdmin",
+    "roles/storage.admin"
+]
+}
+
+resource "google_project_iam_member" "terraform_service_account_confidential_iam" {
+  for_each = toset(var.terraform_service_account_confidential_iam_roles_list)
+  project = google_project.confidential_project.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.terraform_service_account.email}"
+}
+
+/**********************************************
+Create prerequisite groups
+***********************************************/
+
+# TODO
